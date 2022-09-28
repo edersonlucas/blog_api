b2081c5a3e89a734887aa73734ac121b9782ee73
Ederson Lucas <edersonlucas@outlook.com.br>
2022-09-28 01:12:33 -0300

    feat: implement swagger UI in project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 // ...
 
 const loginRouter = require('./routes/login.router');
@@ -9,7 +10,7 @@ const postRouter = require('./routes/post.router');
 const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
