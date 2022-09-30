b762bdbd71d4af78f9c7d6442672a3ab636e72bc
Ederson Lucas <edersonlucas@outlook.com.br>
2022-09-30 17:24:59 -0300

    refactor: Auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,8 +1,12 @@
 const auth = require('../util/auth');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  let { authorization } = req.headers;
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+  if (authorization.includes('Bearer ')) {
+    authorization = authorization.replace('Bearer ', '');
+  }
   try {
     const user = auth.authorization(authorization);
     req.user = user;
